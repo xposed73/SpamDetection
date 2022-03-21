e5e87f9898922d3789eaa33caac87773c995781d
xposed73 <ndmk.edu@gmail.com>
2022-03-21 23:01:25 +0530

    Logistic Regression Added

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 from flask import Flask, render_template, request
 import pickle
 
-# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
-filename = 'spam-sms-mnb-model.pkl'
+# Load the Logistic Regression model and CountVectorizer object from disk
+filename = 'lr-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 cv = pickle.load(open('cv-transform.pkl','rb'))
 app = Flask(__name__)
